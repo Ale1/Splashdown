8025b53ec1bd0a3d8a7added7d3230403b9c7c23
Ale Barbero <alejandro.barbero@gmail.com>
2023-07-12 20:24:42 +0100

    chore: bump package version

--- Assets/Editor/Example.cs
+++ Assets/Editor/Example.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 public static class Example  
 {
         [Splashdown.OptionsProvider]
-        public static Splashdown.Options ProvideSplashdownOptions()
-        {
-            var options = new Splashdown.Options()
-            {
+        public static Splashdown.Options ProvideSplashdownOptions() => new()
+        { 
                 line1 = "Hello",
                 line2 = "Cruel",
                 line3 = "World",
                 textColor = Color.red
-            };
-
-            Debug.Log("Applying dynamic splashdown options");
-            
-            return options;
-        }
+        };
 }
 

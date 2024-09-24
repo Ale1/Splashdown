df877b880bc3a1379649a87a3b1557fcf9b3fdeb
Ale Barbero <alejandro.barbero@gmail.com>
2024-09-24 17:21:02 -0300

    fix: path to test scene

--- Assets/Test/Editor/AutoBuild.cs
+++ Assets/Test/Editor/AutoBuild.cs
@@ -5,11 +5,12 @@ namespace Splashdown.CI
     public class BuildTools
     {
         // Start is called before the first frame update
+        [MenuItem("Splashdown/build")]
         public static void AutoBuild()
         {
             string buildPath = "Builds/iOS";
             
-            BuildPipeline.BuildPlayer(new[] { "Assets/SampleScene.unity" }, buildPath, BuildTarget.iOS, BuildOptions.StrictMode);
+            BuildPipeline.BuildPlayer(new[] { "Assets/Test/SampleScene.unity" }, buildPath, BuildTarget.iOS, BuildOptions.StrictMode);
         }
 
     }
